a2e4f86d0504e571031b9e8b28803cb3a3d326f6
Ryan Gonzalez <rymg19@gmail.com>
2015-11-02 11:53:00 -0600

    Colors

--- fbuildroot.py
+++ fbuildroot.py
@@ -22,7 +22,7 @@ def configure(ctx):
     kw = dict(
         debug=ctx.options.buildtype == 'debug',
         optimize=ctx.options.buildtype == 'release',
-        flags=['-Xclang', '-v'],
+        flags=['-fdiagnostics-color'] if ctx.options.use_color else [],
         platform_options=[
             ({'posix'}, {'external_libs': ['m']})
         ]
