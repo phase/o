5086448e2ffc376a0729531ca31a9e72d448f107
phase <ph4sesaber@gmail.com>
2015-11-08 21:01:40 -0800

    Move web files into /static
    The favicon also works now

--- ide.py
+++ ide.py
@@ -1,24 +1,27 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 def insertFiles(file):
-    file = file.replace("<#include>utilities.html</#include>", open("res/insert/utilities.html").read());
-    file = file.replace("<#include>explanations.js</#include>", open("res/insert/explanations.js").read());
-    file = file.replace("<#include>o.js</#include>", open("res/insert/o.js").read());
+    file = file.replace("<#include>utilities.html</#include>", open("static/insert/utilities.html").read());
+    file = file.replace("<#include>explanations.js</#include>", open("static/insert/explanations.js").read());
+    file = file.replace("<#include>o.js</#include>", open("static/insert/o.js").read());
     return file
 
 @app.route('/')
 def index():
-    index = open("res/index.html").read()
+    url_for('static', filename='logo.ico')
+    index = open("static/index.html").read()
     index = insertFiles(index)
     return index
 
 @app.route("/link/")
 @app.route("/link/<link>")
 def link(link="code=%22Error+in+linking+code%22o&input="):
+    url_for('static', filename='logo.ico')
     print("Link: " + link)
     return render_template("link.html", link=link);
 
