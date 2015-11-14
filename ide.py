929faf0beb8be6947a3aa715959697176526dfcf
phase <ph4sesaber@gmail.com>
2015-11-13 16:38:47 -0800

    Work around for Gunicorn

--- ide.py
+++ ide.py
@@ -1,20 +1,29 @@
 # NOTE: pass -d to this to print debugging info when the server crashes.
 from flask import Flask, render_template, url_for, request
 from subprocess import Popen, PIPE, check_call
-import sys, os, string
+import sys, os, string, glob
 
 app = Flask(__name__)
+
+def compileO():
+    check_call(['gcc', 'o.c', '-DIDE', '-o', 'o-ide', '-lm'])
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     url_for('static', filename='logo.ico')
     if request.method == 'POST':
+        #Check if C was compiled
+        if len(glob.glob("o-ide*")) < 1
+            print("Compiling O...")
+            compileO()
+        #Run code
         code = request.form['code']
         input = request.form['input'].replace('\r\n', '\n')
         print('Got code:', code, 'input:', input)
         print('Running O code...')
         p = Popen(['o-ide', '-e', code], stdout=PIPE, stderr=PIPE, stdin=PIPE, universal_newlines=True)
         output, error = p.communicate(input)
+        #Output to IDE
         print('Output:', output, 'error:', error)
         if p.returncode:
             return render_template('error.html', code=code, input=input, error=error)
@@ -30,9 +39,8 @@ def link(link='code="Error in linking code"o&input='):
     print('Link:', link)
     return render_template('link.html', link=link)
 
-print("__name__:" + __name__)
-if __name__ == 'ide':
+if __name__ == '__main__':
     print('Compiling O...')
-    check_call(['gcc', 'o.c', '-DIDE', '-o', 'o-ide', '-lm'])
+    compileO()
     print('Starting server...')
     app.run(debug='-d' in sys.argv[1:])
