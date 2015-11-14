1cc85e6bef7f92edfb054a78e28fcd79bd19d773
Ryan Rowe <Mego@users.noreply.github.com>
2015-11-13 23:33:09 -0600

    fix ide and compile issues

--- ide.py
+++ ide.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # NOTE: pass -d to this to print debugging info when the server crashes.
 from flask import Flask, render_template, url_for, request
 from subprocess import Popen, PIPE, check_call
@@ -9,17 +10,18 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 def compileO():
-    r = check_call(['gcc', 'o.c', '-DIDE', '-o', 'o-ide', '-lm'])
-    print("o-ide: " + "".join(glob.glob("o-ide*")))
+    r = check_call(['make', 'ide'])
+    print("o-ide: " + "".join(glob.glob("*/o-ide*")))
     if r != 0:
-        print("O code could not be compile. Error: " + r)
+        print("O code could not be compiled. Error: " + r)
+        raise RuntimeError("Could not compile O interpreter")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     url_for('static', filename='logo.ico')
     if request.method == 'POST':
         #Check files that start with 'o-ide*'
-        files = glob.glob("o-ide*")
+        files = glob.glob("*/o-ide*")
         print(files)
         #Check if C was compiled
         if len(files) < 1:
@@ -30,7 +32,7 @@ def index():
         input = request.form['input'].replace('\r\n', '\n')
         print('Got code:', code, 'input:', input)
         print('Running O code...')
-        p = Popen(['o-ide', '-e', code], stdout=PIPE, stderr=PIPE, stdin=PIPE, universal_newlines=True, shell=True)
+        p = Popen([files[0], '-e', code], stdout=PIPE, stderr=PIPE, stdin=PIPE, universal_newlines=True)
         output, error = p.communicate(input)
         #Output to IDE
         print('Output:', output, 'error:', error)
