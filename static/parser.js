875d2ea28128cd2bf5598d4d684e2e142bdf6a87
phase <ph4sesaber@gmail.com>
2015-11-23 06:20:54 -0800

    Support for operator explanations

--- static/parser.js
+++ static/parser.js
@@ -1,3 +1,8 @@
+operators = {
+    'o': "Pop the stack and output",
+    'p': "Pop the stack and print with new line"
+}
+
 ObjType = {
     Number: 0,
     String: 1,
@@ -33,6 +38,27 @@ function newCodeBlock(string){
     return o;
 }
 
+EventType = {
+    ObjectEvent: 0,
+    StringEvent: 1
+}
+var Event = function(){}
+
+function eventString(c, s){
+    var e = new Event();
+    e.string = s;
+    e.c = c;
+    e.type = EventType.StringEvent;
+    return e;
+}
+
+function eventObj(o){
+    var e = new Event();
+    e.object = o;
+    e.type = EventType.ObjectEvent;
+    return e;
+}
+
 var events = [];
 
 //flags
@@ -52,7 +78,7 @@ function parse(code){
         if(fcb && !fs){
             if(c == "}"){
                 fcb = false;
-                events.push(newCodeBlock(buffer));
+                events.push(eventObj(newCodeBlock(buffer)));
                 buffer = "";
                 continue;
             }
@@ -61,15 +87,14 @@ function parse(code){
         if(fs && !fcb) {
             if(c == "\""){
                 fs = false;
-                events.push(newString(buffer));
+                events.push(eventObj(newString(buffer)));
                 buffer = "";
                 continue;
             }
             buffer += c;
         }
         else if(c.match(/[1-9A-F]/)){
-            console.log(c);
-            events.push(newNumber(c));
+            events.push(eventObj(newNumber(c)));
         }
         else if(c == "{"){
             fcb = true;
@@ -77,39 +102,63 @@ function parse(code){
         else if(c == "\""){
             fs = true;
         }
+        else if(c == "J" || c == "K"){
+            events.push(eventString(c, "Assign to variable " + c));
+        }
+        //normal operators
+        else if(c in operators){
+            events.push(eventString(c, operators[c]));
+        }
     }
     var ex = explain(events);
     return ex;
 }
 
-var maxWidth = 0;
+var maxWidth = 0, spaces = 0;
 
 function getMaxWidth() {
     return maxWidth;
 }
 
+function getSpaces(i) {
+    var s = "", j = 0;
+    while(j<i){s+= " ";j++;}
+    return s;
+}
+
 function explain(events) {
     maxWidth = 0;
+    spaces = 1;
     e = "";
-    //console.log(events)
     for(var i = 0; i < events.length; i++){
-        var o = events[i];
-        if(o instanceof Obj){
+        var event = events[i];
+        if(event.type == EventType.ObjectEvent){
+            var o = event.object;
             if(o.type == ObjType.Number){
-                var s = o.num + " Push " + o.num + " to the stack\n";
+                var g = o.num.length;
+                var s = o.num + getSpaces(spaces-g) +  " Push " + o.num + " to the stack\n";
                 if(maxWidth < s.length) maxWidth = s.length;
                 e += s;
             }
             if(o.type == ObjType.String){
-                var s = "\"" + o.string + "\" Push string to the stack\n";
+                var g = o.string.length + 2/*quotes*/;
+                if(spaces < g) spaces = g;
+                var s = "\"" + o.string + "\"" + getSpaces(spaces-g) +  " Push string to the stack\n";
                 if(maxWidth < s.length) maxWidth = s.length;
                 e += s;
             }
             if(o.type == ObjType.CodeBlock){
-                var s = "{" + o.string + "} Push string to the stack\n";
+                var g = o.string.length + 2/*brackets*/;
+                var s = "{" + o.string + "}" + getSpaces(spaces-g) +  " Push codeblock to the stack\n";
                 if(maxWidth < s.length) maxWidth = s.length;
                 e += s;
             }
+        }
+        else if(event.type == EventType.StringEvent){
+            var s = event.string;
+            var g = s.length;
+            if(maxWidth < g) maxWidth = g;
+            e += event.c + getSpaces(spaces) + s + "\n";
         }
     }
     return e;
