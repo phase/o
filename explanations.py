8e06e7a32743f9935f165b17376422b838b33d56
phase <ph4sesaber@gmail.com>
2015-11-28 01:47:27 -0800

    Some changes to explanation generating

--- explanations.py
+++ explanations.py
@@ -1,12 +1,9 @@
-lines = []
-with open('commands.md','rb') as f:
-    for line in f:
-        lines.append(line)
 explanations = "var explanations = {\n"
 for line in open("commands.md"):
     if line.startswith("* _"):
         operator = line.split("(`")[1].split("`)")[0].replace("\\", "\\\\").replace("` ` `", "`").replace("'", "\\'")
         explanation = line.split(": ")[1]
-        explanations += "'" + operator + "':'" + explanation.rstrip("\n\r") + "',\n"
+        explanations += "    '" + operator + "': '" + explanation.rstrip("\n\r") + "',\n"
 with open("static/explanations.js", "wb") as f:
-    f.write(bytes(explanations + "\n};", "UTF-8"))
+    f.write(bytes(explanations + "};", "UTF-8"))
+print("Done making explanations, output in static/explanations.js")
